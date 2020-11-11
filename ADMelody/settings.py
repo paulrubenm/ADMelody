64d90a692705f4b37d638084fe8a6332dddee552
Paul Ruben <paul.ruben.m@gmail.com>
2020-11-11 22:18:38 +0700

    f8

--- ADMelody/settings.py
+++ ADMelody/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'NAME': 'admelody',
         'USER': 'postgres',
         'PASSWORD': 'Thaiminh2912',
-        'HOST': '178.128.23.143',
-        'PORT': '22',
+        'HOST': 'localhost',
+        'PORT': '5244',
     }
 }
 

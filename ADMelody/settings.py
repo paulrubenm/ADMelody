a32b83e76911f3bd57cd590761194fa9a6d19607
Paul Ruben <paul.ruben.m@gmail.com>
2020-11-11 15:55:03 +0700

    finalv3

--- ADMelody/settings.py
+++ ADMelody/settings.py
@@ -143,3 +143,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "anhduongbeauty.2009@gmail.com"
 EMAIL_HOST_PASSWORD = 'Thaiminh2912'
 EMAIL_USE_TLS = True
+
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
